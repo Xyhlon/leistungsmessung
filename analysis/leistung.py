e881b29705a391a882cf40adc00765ec7b43ef14
Maximilian Philipp <philipp@student.tugraz.at>
2022-12-09 13:38:39 +0100

    neue daten und erste diagramme

--- analysis/leistung.py
+++ analysis/leistung.py
@@ -14,30 +14,117 @@ def test_leistung_protokoll():
         "t": r"t",
         "U": r"U",
         "I": r"I",
+        "a": r"a",
+        "b": r"b",
+        "p": r"P",
         "E": r"E_{\mathrm{kin}}",
     }
     gv = {
         "t": r"\si{\second}",
         "U": r"\si{\volt}",
-        "I": r"\si{\cm}",
+        "I": r"\si{\ampere}",
         "E": r"\si{\mega\electronvolt}",
+        "a": r"\si{\volt}",
+        "p": r"\si{\watt}",
+        "b": r"\si{\ohm}",
     }
 
     P = Project("Leistung", global_variables=gv, global_mapping=gm, font=13)
     P.output_dir = "./"
     P.figure.set_size_inches((8, 6))
     ax = P.figure.add_subplot()
-    M1 = 1.5 * 0.24
-    M2 = 1.5 * 0.6
-    m1 = 0.5 * 120
-    m2 = 0.5 * 240
-    Pm1 = 0.5 * 1 + 0.5 * 120
-    Pm2 = 0.5 * 1 + 0.5 * 240
+    Im1 = 0.015 * 0.24
+    Im2 = 0.015 * 0.6
+    Um1 = 0.005 * 120
+    Um2 = 0.005 * 240
+    Pm1 = 0.005 * 1 * 120
+    Pm2 = 0.005 * 1 * 240
 
-    # A1 qualitative Absorption Untersuchung mit und ohne Abschirmung
+    # A1
     filepath = os.path.join(os.path.dirname(__file__), "../data/aufgabe1.csv")
     P.load_data(filepath, loadnew=True)
+    # Setting uncertainties
+    P.data["dU"] = P.data["U"]
+    P.data["dU"][P.data["U"] < 121] = Um1
+    P.data["dU"][P.data["U"] > 121] = Um2
+    P.data["dI"] = P.data["I"]
+    P.data["dI"][P.data["U"] < 121] = Im1
+    P.data["dI"][P.data["U"] > 121] = Im2
+    P.data["dp"] = P.data["p"]
+    P.data["dp"][P.data["U"] < 121] = Pm1
+    P.data["dp"][P.data["U"] > 121] = Pm2
+
+    p = a * I + b * I**2
+
+    P.plot_data(
+        ax,
+        I,
+        p,
+        label="Gemessene Daten",
+        style="#1cb2f5",
+        errors=True,
+    )
+
+    P.plot_fit(
+        axes=ax,
+        x=I,
+        y=p,
+        eqn=p,
+        style=r"#1cb2f5",
+        label="Strom",
+        offset=[0, 30],
+        use_all_known=False,
+        guess={"a": 10, "b": 18},
+        bounds=[
+            {"name": "b", "min": 000, "max": 9000},
+        ],
+        add_fit_params=True,
+        granularity=10000,
+        # gof=True,
+        # scale_covar=True,
+    )
     print(P.data)
+
+    ax.set_title(f"Leistungskennlinie von Strom")
+    P.ax_legend_all(loc=4)
+    ax = P.savefig(f"pIkennlinie.pdf")
+
+    p = a * U + b * U**2
+
+    P.plot_data(
+        ax,
+        U,
+        p,
+        label="Gemessene Daten",
+        style="#1cb2f5",
+        errors=True,
+    )
+
+    P.plot_fit(
+        axes=ax,
+        x=U,
+        y=p,
+        eqn=p,
+        style=r"#1cb2f5",
+        label="Strom",
+        offset=[0, 30],
+        use_all_known=False,
+        guess={"a": 10, "b": 18},
+        bounds=[
+            {"name": "b", "min": 000, "max": 9000},
+        ],
+        add_fit_params=True,
+        granularity=10000,
+        # gof=True,
+        # scale_covar=True,
+    )
+    print(P.data)
+
+    ax.set_title(f"Leistungskennlinie von Spannung")
+    P.ax_legend_all(loc=4)
+    ax = P.savefig(f"pUkennlinie.pdf")
+    # A3 Darstellung der Zählstatistik
+    P.vload()
 
 
 if __name__ == "__main__":
